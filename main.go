d062a5d0f97b6bbdebd9ea2516bc2e9f22e9b7d3
AndFran <andrewwilliamfranks@gmail.com>
2024-01-07 21:17:31 +0100

    Update main.go - query db

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
+func queryDatabase(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM person")
+	if err == nil {
+		for rows.Next() {
+			var id int
+			var firstName string
+			var lastName string
+			rows.Scan(&id, &firstName, &lastName)
+			fmt.Println(id, firstName, lastName)
+		}
+	} else {
+		log.Fatal(err.Error())
+	}
+}
+
 func main() {
 	listDrivers()
 	db, err := openDatbase()
 	if err == nil {
+		queryDatabase(db)
 		db.Close()
 	} else {
 		panic(err)
